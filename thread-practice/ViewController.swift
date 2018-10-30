c25fe511e78d7bf3308a017ba05d04397558b63f
Shima <shima.jin@icloud.com>
2018-10-30 15:29:11 +0900

    Initial Commit

---
+++ thread-practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thread-practice
+//
+//  Created by Jinsei Shima on 2018/10/30.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
